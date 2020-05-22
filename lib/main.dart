234a4211c0d111e24043d4bdee5c248db8d18ead
Bodhish Thomas <bodhish@gmail.com>
2020-05-22 16:52:42 +0530

    change appicons

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.blueGrey,
-    appBar: AppBar(
-      backgroundColor: Colors.blueGrey[900],
-      title: Text(
-        'Bodhi Creations',
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text(
+          'Bodhi Creations',
 //        style: TextStyle(fontStyle: FontStyle.italic),
+        ),
       ),
+      body: Center(child: Image(image: AssetImage('images/bodhi.png'))),
     ),
-    body: Center(child: Image(image: AssetImage('images/bodhi.png'))),
-  )));
+    debugShowCheckedModeBanner: false,
+  ));
 }
